a71aed50460d256cd54a327687d7bf3cb3cdd052
Adam Weitzman <adamcweitzman@gmail.com>
2015-11-03 19:20:18 -0600

    site working

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
-// var mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGO_DB_PERSONAL_SITE);
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB_CONN_PERSONAL_SITE);

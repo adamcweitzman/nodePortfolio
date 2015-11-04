891acb046c86cf936165b70c6b0ecf6dc6e5eb12
Adam Weitzman <adamcweitzman@gmail.com>
2015-11-03 22:08:12 -0600

    database connected

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Likes = require('../model/likes');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/likes', function(req, res, next) {
+	Likes.find({}, function(err, likes){
+		var likes = likes
+		res.json(likes)
+	});
 });
 
 module.exports = router;
